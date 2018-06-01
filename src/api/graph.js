b119fa6cfaf9164e37943c079342305e1fcc0718
Anastasia <a.shemetillo@gmail.com>
2018-06-01 19:57:43 +0300

    graph initial commit

---
+++ src/api/graph.js
@@ -0,0 +1,14 @@
+export default class GraphApi {
+
+  /**
+   * Return graph
+   * @returns {object}
+   */
+  static async getGraph() {
+    // const url = `${apiUrl}/getGraph`;
+    // const response = await fetch(url);
+    // const results = await response.json();
+    // return results;
+    return [];
+  }
+}
